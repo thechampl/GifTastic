ada859dafcd1707509e9a5ddbf0aaf966f997025
Luke Champ <luke.champ@outlook.com>
2018-01-26 17:24:46 -0500

    need still on click event

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,53 +1,68 @@
 
 var animalList = [
-"Cat", "Dog", "Tiger", "Lion", "Zebra", "Rhino", "Dolphin", "Kangaroo"
+    "Cat", "Dog", "Tiger", "Lion", "Zebra", "Rhino", "Dolphin", "Kangaroo"
 ]
 
+function buttonrender() {
+    $("#animalbuttons").empty();
+    for (var i = 0; i < animalList.length; i++) {
+        var button = $("<button class=animal>").text(animalList[i]);
+        button.attr("data-animal", animalList[i]);
+        $("#animalbuttons").append(button);
+    }
 
-for (var i = 0; i < animalList.length; i++){
-    var button = $("<button>").text(animalList[i]); 
-    button.attr("data-animal", animalList[i])
-    $("#animalbuttons").append(button);
 
 }
 
+var userSearch = $("#userSearch");
 
-$("button").on("click", function() {
+$("#search").on("click", function () {
+    console.log(userSearch.val());
+    event.preventDefault();
+    animalList.push(userSearch.val());
+    console.log(animalList);
+    buttonrender();
+
+});
+
+$("#animalbuttons").on("click", ".animal", function () {
+    $("#divplace").empty();
+
     var animal = $(this).attr("data-animal");
+    console.log(animal);
 
-    
+
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+        animal + "&api_key=dc6zaTOxFJmzC&limit=10&rating=g";
 
-    
+    console.log(queryURL)
+
+
+
     $.ajax({
         url: queryURL,
         method: "GET"
-      })
-   
-      .done(function(response) {
-        var results = response.data;        
-        for (var i = 0; i < results.length; i++) {
+    })
 
-            
-            var gifDiv = $("<div>");
+        .done(function (response) {
+            var results = response.data;
+            for (var i = 0; i < results.length; i++) {
+                var gifDiv = $("<div class='col-md-3'>");
+                var animalImage = $("<img class='img-thumbnail'>");
+                animalImage.attr("src", results[i].images.fixed_height.url);
+                gifDiv.append(animalImage);
+                $("#divplace").prepend(gifDiv);
+            }
 
-            
-
-            // Creating an image tag
-            var animalImage = $("<img>");
-
-            // Giving the image tag an src attribute of a proprty pulled off the
-            // result item
-            animalImage.attr("src", results[i].images.fixed_height.url);
-
-            // Appending the paragraph and personImage we created to the "gifDiv" div we created
-            
-            gifDiv.append(animalImage);
-
-            // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-            $("#divplace").prepend(gifDiv);
-          }
-        
         });
-    });
+});
+
+
+
+
+$('img-thumbnail').on("click", function () {
+    alert("test");
+    animalImage.attr("src", results[i].images.fixed_height_still.url);
+
+});
+buttonrender()
